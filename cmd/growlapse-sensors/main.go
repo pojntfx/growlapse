d4fd3c77d8228cd47b84d05e4aa856031f4d0158
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-05-02 11:10:56 +0200

    feat: Add basic I2C sensor client

---
+++ cmd/growlapse-sensors/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/d2r2/go-bsbmp"
+	"github.com/d2r2/go-i2c"
+	"github.com/d2r2/go-logger"
+)
+
+func main() {
+	i2cBusFlag := flag.Int("i2cBus", 1, "I2C bus number; find with i2cdetect -l")
+	i2cAddressFlag := flag.String("i2cAddress", "0x77", "I2C address of the sensor; find with i2cdetect -y your-i2c-bus-number")
+	verboseFlag := flag.Bool("verbose", false, "Enable verbose logging")
+
+	i2cAddress, err := strconv.ParseUint(strings.Replace(*i2cAddressFlag, "0x", "", -1), 16, 8)
+	if err != nil {
+		panic(err)
+	}
+
+	flag.Parse()
+
+	log.Printf("starting with I2C bus %v and I2C address %v", *i2cBusFlag, *i2cAddressFlag)
+
+	i2c, err := i2c.NewI2C(uint8(i2cAddress), *i2cBusFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer i2c.Close()
+
+	if *verboseFlag {
+		logger.ChangePackageLogLevel("i2c", logger.DebugLevel)
+		logger.ChangePackageLogLevel("bsbmp", logger.DebugLevel)
+	} else {
+		logger.ChangePackageLogLevel("i2c", logger.FatalLevel)
+		logger.ChangePackageLogLevel("bsbmp", logger.FatalLevel)
+	}
+
+	sensor, err := bsbmp.NewBMP(bsbmp.BMP180, i2c)
+	if err != nil {
+		panic(err)
+	}
+
+	id, err := sensor.ReadSensorID()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("using sensor ID %v", id)
+
+	if err := sensor.IsValidCoefficients(); err != nil {
+		panic(err)
+	}
+
+	temperature, err := sensor.ReadTemperatureC(bsbmp.ACCURACY_ULTRA_HIGH)
+	if err != nil {
+		panic(err)
+	}
+
+	pressure, err := sensor.ReadPressurePa(bsbmp.ACCURACY_ULTRA_HIGH)
+	if err != nil {
+		panic(err)
+	}
+
+	altitude, err := sensor.ReadAltitude(bsbmp.ACCURACY_ULTRA_HIGH)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Temperature: %v °C\nPressure: %v Pa\nAltitude: %v m\n", temperature, pressure, altitude)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.15
 
 require (
 	github.com/blackjack/webcam v0.0.0-20200313125108-10ed912a8539
+	github.com/d2r2/go-bsbmp v0.0.0-20190515110334-3b4b3aea8375
+	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc
+	github.com/d2r2/go-logger v0.0.0-20181221090742-9998a510495e
 	github.com/kataras/compress v0.0.6
 	github.com/maxence-charriere/go-app/v9 v9.0.0-20210424172519-841eccd01cdf
 	github.com/robfig/cron/v3 v3.0.1
