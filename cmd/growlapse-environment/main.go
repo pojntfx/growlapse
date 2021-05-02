2270eb0a31b436a375a57407733512ecf461d47f
Felix Pojtinger <felix@pojtinger.com>
2021-05-02 13:40:50 +0200

    feat: Add temperature and capacitance registers

--- cmd/growlapse-environment/main.go
+++ cmd/growlapse-environment/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/d2r2/go-bsbmp"
 	"github.com/d2r2/go-i2c"
@@ -16,6 +16,7 @@ func main() {
 	i2cBusFlag := flag.Int("i2cBus", 1, "I2C bus number; find with i2cdetect -l")
 	i2cAddressFlag := flag.String("i2cAddress", "0x77", "I2C address of the sensor; find with i2cdetect -y your-i2c-bus-number")
 	verboseFlag := flag.Bool("verbose", false, "Enable verbose logging")
+	readIntervalFlag := flag.Int("readInterval", 2000, "Interval (in ms) in which to re-read values")
 
 	i2cAddress, err := strconv.ParseUint(strings.Replace(*i2cAddressFlag, "0x", "", -1), 16, 8)
 	if err != nil {
@@ -56,20 +57,24 @@ func main() {
 		panic(err)
 	}
 
-	temperature, err := sensor.ReadTemperatureC(bsbmp.ACCURACY_ULTRA_HIGH)
-	if err != nil {
-		panic(err)
-	}
+	for {
+		temperature, err := sensor.ReadTemperatureC(bsbmp.ACCURACY_ULTRA_HIGH)
+		if err != nil {
+			panic(err)
+		}
 
-	pressure, err := sensor.ReadPressurePa(bsbmp.ACCURACY_ULTRA_HIGH)
-	if err != nil {
-		panic(err)
-	}
+		pressure, err := sensor.ReadPressurePa(bsbmp.ACCURACY_ULTRA_HIGH)
+		if err != nil {
+			panic(err)
+		}
 
-	altitude, err := sensor.ReadAltitude(bsbmp.ACCURACY_ULTRA_HIGH)
-	if err != nil {
-		panic(err)
-	}
+		altitude, err := sensor.ReadAltitude(bsbmp.ACCURACY_ULTRA_HIGH)
+		if err != nil {
+			panic(err)
+		}
 
-	fmt.Printf("Temperature: %v °C\nPressure: %v Pa\nAltitude: %v m\n", temperature, pressure, altitude)
+		log.Printf("Temperature: %v °C Pressure: %v Pa Altitude: %v m", temperature, pressure, altitude)
+
+		time.Sleep(time.Millisecond * time.Duration(*readIntervalFlag))
+	}
 }
--- cmd/growlapse-moisture/main.go
+++ cmd/growlapse-moisture/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -18,7 +17,9 @@ var (
 )
 
 const (
-	ReadLightRegister = byte(4)
+	LightRegister       = byte(4)
+	CapacitanceRegister = byte(0)
+	TemperatureRegister = byte(5)
 
 	ResetTimeout                   = time.Millisecond * 10
 	RequestLightMeasurementTimeout = time.Millisecond * 100
@@ -28,8 +29,10 @@ func main() {
 	i2cBusFlag := flag.Int("i2cBus", 1, "I2C bus number; find with i2cdetect -l")
 	i2cAddressFlag := flag.String("i2cAddress", "0x20", "I2C address of the sensor; find with i2cdetect -y your-i2c-bus-number")
 	verboseFlag := flag.Bool("verbose", false, "Enable verbose logging")
-	resetTimeout := flag.Int("resetTimeout", 10, "Timeout (in milliseconds) to wait between resetting the sensor")
-	lightMeasurementTimeout := flag.Int("lightMeasurementTimeout", 1000, "Timeout (in milliseconds) to wait for light measurement to complete")
+	resetTimeout := flag.Int("resetTimeout", 10, "Timeout (in ms) to wait between resetting the sensor")
+	readyTimeout := flag.Int("readyTimeout", 100, "Timeout (in ms) to wait for the sensor to be ready")
+	lightMeasurementTimeout := flag.Int("lightMeasurementTimeout", 1000, "Timeout (in ms) to wait for light measurement to complete")
+	readIntervalFlag := flag.Int("readInterval", 2000, "Interval (in ms) in which to re-read values")
 
 	i2cAddress, err := strconv.ParseUint(strings.Replace(*i2cAddressFlag, "0x", "", -1), 16, 8)
 	if err != nil {
@@ -66,18 +69,32 @@ func main() {
 		break
 	}
 
+	time.Sleep(time.Millisecond * time.Duration(*readyTimeout))
+
 	for {
+		temperature, err := i2c.ReadRegU16BE(TemperatureRegister)
+		if err != nil {
+			panic(err)
+		}
+
+		capacitance, err := i2c.ReadRegU16BE(CapacitanceRegister)
+		if err != nil {
+			panic(err)
+		}
+
 		if _, err := i2c.WriteBytes(RequestLightMeasurement); err != nil {
 			panic(err)
 		}
 
 		time.Sleep(time.Millisecond * time.Duration(*lightMeasurementTimeout))
 
-		lightLevel, err := i2c.ReadRegU16LE(ReadLightRegister)
+		lightLevel, err := i2c.ReadRegU16BE(LightRegister)
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Printf("Light level: %v (no unit)\n", lightLevel)
+		log.Printf("Temperature: %v (no unit) Capacitance: %v (no unit) Light level: %v (no unit)", temperature, capacitance, lightLevel)
+
+		time.Sleep(time.Millisecond * time.Duration(*readIntervalFlag))
 	}
 }
