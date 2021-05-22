ef225a99d2eff816f5e8f28d7ef8c25fc32e10e8
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-05-22 18:33:45 +0200

    feat: Add prototype of I2C twi utilities

--- tinygo/main.go
+++ tinygo/main.go
@@ -1,8 +1,14 @@
 package main
 
+/*
+#cgo CFLAGS: -I/usr/avr/include
+#cgo LDFLAGS: -Lavr
+#include "twislave.h"
+*/
+import "C"
+
 import (
 	"machine"
-	"time"
 )
 
 func main() {
@@ -12,13 +18,30 @@ func main() {
 		Pin: machine.ADC0,
 	}
 
-	adc.Configure()
+	adc.Configure(machine.ADCConfig{})
+
+	var address uint8
+	address = 0x5
+
+	println("initing i2c")
+
+	// C.I2C_setCallbacks(C.I2C_received, C.I2C_requested)
+	C.init_twi_slave(address)
+	// C.I2C_init(address)
+
+	println("done initing i2c")
 
 	for {
-		v := adc.Get()
-
-		println("Moisture level: ", v)
-
-		time.Sleep(time.Millisecond * 500)
+		print(len(C.i2cdata))
 	}
+
+	// var v uint16
+
+	// for {
+	// 	v = adc.Get()
+
+	// 	println(v)
+
+	// 	time.Sleep(time.Millisecond * 500)
+	// }
 }
