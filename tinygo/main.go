c47a0d8c57703bba1db9e41238e30a76264e55dd
Felix Pojtinger <felix@pojtinger.com>
2021-05-23 12:38:06 +0200

    feat: Start working on I2C slave mode

--- tinygo/main.go
+++ tinygo/main.go
@@ -3,45 +3,16 @@ package main
 /*
 #cgo CFLAGS: -I/usr/avr/include
 #cgo LDFLAGS: -Lavr
-#include "twislave.h"
+#include "i2c.h"
 */
 import "C"
 
-import (
-	"machine"
-)
-
 func main() {
-	machine.InitADC()
+	println("Initing I2C")
 
-	adc := machine.ADC{
-		Pin: machine.ADC0,
-	}
+	C.i2c_init(0x10)
 
-	adc.Configure(machine.ADCConfig{})
+	println("Stopping I2C")
 
-	var address uint8
-	address = 0x5
-
-	println("initing i2c")
-
-	// C.I2C_setCallbacks(C.I2C_received, C.I2C_requested)
-	C.init_twi_slave(address)
-	// C.I2C_init(address)
-
-	println("done initing i2c")
-
-	for {
-		print(len(C.i2cdata))
-	}
-
-	// var v uint16
-
-	// for {
-	// 	v = adc.Get()
-
-	// 	println(v)
-
-	// 	time.Sleep(time.Millisecond * 500)
-	// }
+	C.i2c_stop()
 }
